732442b022e822816836303004c3d25e8fe12112
Gregory Singleton <yadig69design@gmail.com>
2020-06-16 12:01:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -61,3 +61,35 @@ console.log(parseInt('nonsense', 10))
 console.log(parseInt('2.2222', 10))
 
 console.log(parseFloat('80.123999'))
+
+function add(a,b){
+  return a + b;
+}
+
+function subtract(a,b){
+  return a - b;
+}
+
+function multiply(a,b){
+  return a * b;
+}
+
+function divide(a,b){
+  return a / b;
+}
+
+function inc(n){
+  return n += 1;
+}
+
+function dec(n){
+  return n -= 1;
+}
+
+function makeInt(n){
+  return parseInt(n, 10);
+}
+
+function preserveDecimal(n){
+  return parseFloat(n);
+}
