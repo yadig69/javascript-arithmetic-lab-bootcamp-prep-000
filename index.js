6b8e5cf1329b0d7e36312c3770d5f6d001c3d7cf
Gregory Singleton <yadig69design@gmail.com>
2020-06-16 10:53:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 // var number = 5
-// 
+//
 // number++
-// 
+//
 // number
-// 
+//
 // number--
-// 
+//
 // number
-// 
+//
 // number += 3
 // // number -= 2
 // number *= 10
 // // number /= 5
-// 
+//
 // console.log(number++)
 
 
@@ -20,6 +20,10 @@ var number = 10
 
 function add5() {
   number += 5
+}
+
+function divideBy3() {
+  number /= 3
 }
 
 console.log(number)
