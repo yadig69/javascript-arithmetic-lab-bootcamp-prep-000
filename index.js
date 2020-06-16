f7a7e4968a38f0aa058e002d8875ba248c13f0f8
Gregory Singleton <yadig69design@gmail.com>
2020-06-16 10:49:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-var number = 5
-
-number++
-
-number
-
-number--
-
-number
-
-number += 3
-// number -= 2
-number *= 10
-// number /= 5
-
-console.log(number++)
+// var number = 5
+// 
+// number++
+// 
+// number
+// 
+// number--
+// 
+// number
+// 
+// number += 3
+// // number -= 2
+// number *= 10
+// // number /= 5
+// 
+// console.log(number++)
